0ac848480d30a60d7e45857c38a96693422ac514
Thibault PIANA <thibault.piana@imt-atlantique.net>
2018-02-18 03:20:52 +0100

    Finish add form

---
+++ TP/pages/admin/homepage.php
@@ -0,0 +1,10 @@
+<?php include "./components/directory-box.php"; ?>
+<?php include "./components/directory-head-list.php"; ?>
+
+
+<div class="section-body">
+    <?php
+        generateHeadList(true);
+    ?>
+
+</div>
